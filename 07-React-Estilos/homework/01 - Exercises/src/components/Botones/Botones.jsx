0db1bc65409c4f7fb199118a603ca6be5a2e4378
NoistNT <arielgnr23@gmail.com>
2023-02-07 16:30:57 -0300

    Finished Homework

--- 07-React-Estilos/homework/01 - Exercises/src/components/Botones/Botones.jsx
+++ 07-React-Estilos/homework/01 - Exercises/src/components/Botones/Botones.jsx
@@ -1,17 +1,6 @@
 import React from 'react'
-import styled from 'styled-components'
+import { DivButtons, Buttons } from '../styled-components/StyledButtons'
 
-const DivButtons = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-around;
-  align-items: center;
-`
-const Buttons = styled.button`
-  border-radius: 5px;
-  margin: 10px;
-  padding: 5px;
-`
 
 export default class Botones extends React.Component {
   render () {
---
+++ 07-React-Estilos/homework/01 - Exercises/src/components/styled-components/StyledButtons.jsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+const DivButtons = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+`
+
+const Buttons = styled.button`
+  border-radius: 5px;
+  margin: 10px;
+  padding: 5px;
+`
+
+export {Buttons, DivButtons}
