ebbf3061a6c48282dea5e1fb9cf349c9507fa938
NoistNT <arielgnr23@gmail.com>
2023-02-06 23:41:03 -0300

    Finished Homework

--- 06-React-Intro/homework/01 - Exercises/src/components/Botones.jsx
+++ 06-React-Intro/homework/01 - Exercises/src/components/Botones.jsx
@@ -8,7 +8,7 @@ class Botones extends React.Component {
   render () {
     return (
       <div>
-        <button onClick={() => alert(this.props.alerts.m1)}>Modulo 1</button>
+        <button onClick={() => alert(this.props.alerts.m1)}>Módulo 1</button>
         <button onClick={() => alert(this.props.alerts.m2)}>Módulo 2</button>
       </div>
     )
