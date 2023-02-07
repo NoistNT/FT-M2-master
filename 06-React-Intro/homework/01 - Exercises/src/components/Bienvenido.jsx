9e0d7684279a513eb24e1fe3a4e05acfce5b6948
NoistNT <arielgnr23@gmail.com>
2023-02-06 23:37:39 -0300

    Finished Homework

--- 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
+++ 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React from 'react'
+import Botones from './Botones'
 
-const studentName = "";
-const techSkills = ["Html", "Css", "JavaScript", "React", "Redux"];
-const alerts = { m1: "Aprobado", m2: "En curso" };
+const studentName = 'Ariel Piazzano'
+const techSkills = ['Html', 'Css', 'JavaScript', 'React', 'Redux']
+const alerts = { m1: 'Aprobado', m2: 'En curso' }
 
-export default function Bienvenido() {
-  // el código de tu componente acá
-  return;
+export default function Bienvenido () {
+  return (
+    <div>
+      <h1>Bienvenido a Soy Henry!</h1>
+      <h3>{studentName}</h3>
+      <ul>
+        {
+          techSkills.map((skill, index) => <li key={index}> {skill} </li>)
+        }
+      </ul>
+        <Botones alerts={alerts}/>
+    </div>
+  )
 }
 
 // Esto lo exportamos para los tests
-export { studentName, techSkills, alerts };
+export { studentName, techSkills, alerts }
---
+++ 06-React-Intro/homework/01 - Exercises/src/components/Botones.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class Botones extends React.Component {
+  // constructor (props) {
+  //   super(props)
+  // }
+
+  render () {
+    return (
+      <div>
+        <button onClick={() => alert(this.props.alerts.m1)}>Modulo 1</button>
+        <button onClick={() => alert(this.props.alerts.m2)}>Módulo 2</button>
+      </div>
+    )
+  }
+}
+
+export default Botones
