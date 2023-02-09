1d8bfa2ea797f836fd114f204656ff315d7b12eb
NoistNT <arielgnr23@gmail.com>
2023-02-09 01:10:37 -0300

    Homework Finished

--- 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Animals/Animals.jsx
+++ 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Animals/Animals.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
 
 export default class Animals extends React.Component {
+  // eslint-disable-next-line no-useless-constructor
   constructor (props) {
-    super()
+    super(props)
   }
 
   render () {
-    return <div></div>
+    return (
+    <div>
+      {
+        this.props.animals.map((animal, index) => {
+          return (
+            <div key={index}>
+              <h5>{animal.name}</h5>
+              <img src={animal.image} alt={animal.name} style={{width: '300px'}}/>
+              <br />
+              <span>{animal.specie}</span>
+            </div>
+          )
+          
+    })}
+    </div>
+    )
   }
 }
+
--- 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Zoo/Zoo.jsx
+++ 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Zoo/Zoo.jsx
@@ -1,15 +1,60 @@
-import React from 'react';
-// eslint-disable-next-line no-unused-vars
-import Animals from '../Animals/Animals';
-// eslint-disable-next-line no-unused-vars
-import Species from '../Species/Species';
-import './Zoo.module.css';
+/* eslint-disable no-unused-vars */
+import React from 'react'
+// import { useState } from 'react'
+import Animals from '../Animals/Animals'
+import Species from '../Species/Species'
+import './Zoo.module.css'
 
 export default function Zoo() {
-   /* Escribe acá tu código */
+
+   const [zoo, setZoo] = React.useState({
+      zooName: '',
+      animals: [],
+      species: [],
+      allAnimals: []
+   })
+
+   const handleInputChange = (e) => {
+      setZoo({ ...zoo, zooName: e.target.value})
+   }
+
+   const handleSpecies = (e) => {
+      setZoo({ ...zoo,
+         animals: zoo.allAnimals.filter(animal => animal.specie === e.target.value)
+      })
+   }
+   
+   const handleAllSpecies = () => {
+      setZoo({ ...zoo, animals: zoo.allAnimals, })
+   }
+   
+   React.useEffect( () => {
+      fetch('http://localhost:3001/zoo')
+         .then((result) => result.json())
+         .then((data) => {
+            setZoo({
+               ...zoo,
+               animals: data.animals,
+               species: data.species,
+               allAnimals: data.animals,
+            })
+         })
+         .catch((error) => console.log(error))
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      }, []
+   )
+
    return (
       <div>
-         <h1>-</h1>
+         <label>Zoo Name:</label>
+         <input onChange={handleInputChange} value={zoo.zooName} />
+         <h1>{ zoo.zooName }</h1>
+         <Species 
+            species={zoo.species}
+            handleSpecies={handleSpecies}
+            handleAllSpecies={handleAllSpecies}
+         />
+         <Animals animals={zoo.animals}/>
       </div>
    );
 }
